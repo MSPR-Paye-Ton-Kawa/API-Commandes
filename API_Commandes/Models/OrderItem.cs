c97fb91bec5a13b030363e9d8a51e93ff4d6737d
Colas Rabiller <colas.rabiller@gmail.com>
2024-08-03 16:58:55 +0200

    rebase on dev

---
+++ API_Commandes/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Commandes.Models
+{
+        public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }  // Managed externally
+        public int Quantity { get; set; }
+    }
+   
+}
